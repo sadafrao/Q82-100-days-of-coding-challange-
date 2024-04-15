7a770d6539cdf29114f6cfe7d8e7f7f130423fa1
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:17:28 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,8 @@
+// Q82
+// this function count the character in string by .length
+function  lengthstring(str : string): number{
+    return str.length;
+
+}
+console.log(lengthstring("sadaf"));
+// output is 5
